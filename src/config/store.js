064e5e2e4d46ab7d9f24c11cf78bcaaafa25d81d
Brian Camp <brian.camp.hv@gmail.com>
2021-02-19 13:23:02 -0600

    added firebase config

--- src/config/store.js
+++ src/config/store.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyCjoChnlzEDOt2eeoXSGdZqrXdsnRrAC7Y",
-    authDomain: "mystorage-cd584.firebaseapp.com",
-    databaseURL: "https://mystorage-cd584.firebaseio.com",
-    projectId: "mystorage-cd584",
-    storageBucket: "mystorage-cd584.appspot.com",
-    messagingSenderId: "919282323349"
-  };
+  apiKey: "AIzaSyDAB7xpdUb6APaLmgwD8A_Ny37vA2bSZaQ",
+  authDomain: "crwn-db-743d6.firebaseapp.com",
+  databaseURL: "https://crwn-db-743d6-default-rtdb.firebaseio.com",
+  projectId: "crwn-db-743d6",
+  storageBucket: "crwn-db-743d6.appspot.com",
+  messagingSenderId: "81613914964",
+  appId: "1:81613914964:web:f44b3d3f6a7422ca57fec1",
+  measurementId: "G-80BLW3QK8X"
+};
 
-  const MyStore = firebase.initializeApp(config);
+const MyStore = firebase.initializeApp(config);
 
-  export default MyStore;
+export default MyStore;
